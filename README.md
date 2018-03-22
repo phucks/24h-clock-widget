90b0d3adcad8c12bae88514c12f922d51d5423a9
phucks <mail@toubs.de>
2018-03-22 15:47:23 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+24h Clock Desktop Widget
+===
+
+## Still in development!
+
+Made with Node and Electron.
+
+How to run:
+
+``` bash
+npm install
+tsc *.ts # Compiles all typescript files to JavaScript
+npm start
+```
---
+++ main.ts
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require("electron")
+const path = require("path")
+const url = require("url")
+
+function createWindow() {
+    let win = new BrowserWindow({
+        width: 600,
+        height: 600,
+        frame: false,
+        transparent: true,
+        toolbar: false
+    })
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "main.html"),
+        protocol: "file:",
+        slashes: true
+    }))
+    win.show()
+}
+
+
+app.on("ready", createWindow)
